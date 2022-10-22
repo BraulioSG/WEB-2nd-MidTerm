8616a45b80d799861d371f0b9549b27bdfaa7800
Braulio Solorio <soloriobraulio03@gmail.com>
2022-10-21 20:03:03 -0500

    :)

--- src/router.js
+++ src/router.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const api = require('./api');
+
 //middleware (every request passes through here)
 router.use((req, _res, next) => {
+    let now = new Date();
     console.log(
         `new request: http:/localhost:${process.env.port || 3000}${req.url} `,
-        Date.now()
+        Date.UTC(now.getMilliseconds())
     );
     next();
 });
@@ -13,10 +15,10 @@ router.use((req, _res, next) => {
 //defining the routes
 
 //home route
-router.get('/', (_req, res) => {
-    let response = api.get();
-    console.log(response);
-    res.render(`${__dirname}/views/index`, { result: response });
+router.get('/', (_req, res, next) => {
+    api.get('all').then((response) => {
+        res.render(`${__dirname}/views/index`, { result: response });
+    });
 });
 
 module.exports = router;
