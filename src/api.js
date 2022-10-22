5edea67219ef864f050ab12061793ea281ed728b
Braulio Solorio <soloriobraulio03@gmail.com>
2022-10-22 18:21:53 -0500

    almost done

--- src/api.js
+++ src/api.js
@@ -2,22 +2,44 @@ const https = require('https');
 
 require('dotenv').config();
 
-function get(path) {
-    const API_URL = `https://akabab.github.io/superhero-api/api/${path}.json`;
-    return new Promise((resolve) => {
-        https
-            .get(API_URL, (res) => {
-                let response = '';
-                res.on('data', (data) => {
-                    response += data;
-                });
-                res.on('end', () => {
-                    response = JSON.parse(response);
-                    resolve(response);
-                });
-            })
-            .on('error', (err) => resolve(err));
-    });
-}
+module.exports = {
+    get: (path) => {
+        const API_URL = `https://akabab.github.io/superhero-api/api/${path}.json`;
+        return new Promise((resolve) => {
+            https
+                .get(API_URL, (res) => {
+                    let response = '';
+                    res.on('data', (data) => {
+                        response += data;
+                    });
+                    res.on('end', () => {
+                        response = JSON.parse(response);
+                        resolve(response);
+                    });
+                })
+                .on('error', (err) => resolve(err));
+        });
+    },
 
-exports.get = get;
+    getById: (id) => {
+        const API_URL = `https://akabab.github.io/superhero-api/api/id/${id}.json`;
+        return new Promise((resolve) => {
+            https
+                .get(API_URL, (res) => {
+                    let response = '';
+                    res.on('data', (data) => {
+                        response += data;
+                    });
+                    res.on('end', () => {
+                        try {
+                            response = JSON.parse(response);
+                            resolve(response);
+                        } catch {
+                            resolve(null);
+                        }
+                    });
+                })
+                .on('error', (err) => resolve(err));
+        });
+    },
+};
